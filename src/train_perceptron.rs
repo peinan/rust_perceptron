d20067ff707dd55ad9887e8f1ff25cf7a7ac63c4
peinan <peinan7@gmail.com>
2017-05-18 23:56:37 +0900

    :pencil2: updated

--- src/train_perceptron.rs
+++ src/train_perceptron.rs
@@ -2,16 +2,14 @@
 /// Author:     Peinan ZHANG
 /// Created at: 2017-05-12
 
-use std::io::{self, BufReader, Lines};
+use std::io::{BufReader, Lines};
 use std::io::prelude::*;
 use std::fs::File;
 use std::error::Error;
-use std::path::Path;
 use std::collections::HashMap;
 
 
 static N_EPOCH: u32 = 10;
-static mut WORD_MODEL: HashMap<String, i32> = HashMap::new();
 
 /// Load inputs to lines
 fn load_data(path: String) -> Lines<BufReader<File>> {
@@ -45,15 +43,15 @@ fn parse_line(line: String) -> (i8, HashMap<String, u8>) {
 
 
 /// Predict one set
-fn predict_one(x: HashMap<String, u8>, W: HashMap<String, i32>) -> i8 {
-    let mut Wx: i32 = 0;
-    for (word, count) in x.iter() {
-        Wx += match W.contains_key(word) {
-            true => W[word] * (x[word] as i32),
+fn predict_one(x: &HashMap<String, u8>, w: &HashMap<String, i32>) -> i8 {
+    let mut wx: i32 = 0;
+    for (word, _) in x.iter() {
+        wx += match w.contains_key(word) {
+            true => w[word] * (x[word] as i32),
             false => 0,
         };
     }
-    let y_ = sign((Wx as f64));
+    let y_ = sign((wx as f64));
 
     y_
 }
@@ -70,9 +68,42 @@ fn sign(x: f64) -> i8 {
 
 
 fn main() {
+    let mut word_model: HashMap<String, i32> = HashMap::new();
     let in_fp = "data/titles-en-train.small.labeled";
+
     for line in load_data(in_fp.to_string()) {
         let (y, x) = parse_line(line.unwrap());
-        let y_ = predict_one(x, WORD_MODEL);
+        let y_pred = predict_one(&x, &word_model);
+        if y_pred != y {
+            match y_pred {
+                1 => {
+                    for (word, _) in x.iter() {
+                        // println!("{}", word);
+                        // let weight = word_model.entry(word).or_insert(0);
+                        // *weight += -1;
+                        word_model[word] = match x.contains_key(word) {
+                            true => word_model[word] + -1,
+                            false => 0,
+                        }
+                    }
+                }
+                -1 => {
+                    for (word, _) in x.iter() {
+                        println!("{}", word);
+                        // let weight = word_model.entry(word).or_insert(0);
+                        // *weight += 1;
+                        word_model[word] = match x.contains_key(word) {
+                            true => word_model[word] + 1,
+                            false => 0,
+                        }
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
+
+    for (k, v) in word_model.iter() {
+        println!("{}: {}", k, v);
     }
 }
